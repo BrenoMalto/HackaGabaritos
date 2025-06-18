4be993d9513444bce44f0342ddee8ed24bc915e2
Ryan1590 <ryanneno1590@gmail.com>
2025-06-18 08:49:15 -0300

    Estrutura base

---
+++ src/main/java/Projeto/example/HackaGabaritos/HackaGabaritosApplication.java
@@ -0,0 +1,13 @@
+package Projeto.example.HackaGabaritos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackaGabaritosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackaGabaritosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projeto/example/HackaGabaritos/HackaGabaritosApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.example.HackaGabaritos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackaGabaritosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
